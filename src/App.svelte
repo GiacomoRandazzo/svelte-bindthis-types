52ebd3b597b9c4b080f068abfee5e349f3c47d8d
Giacomo Randazzo <giacomoran@gmail.com>
2022-09-15 19:08:30 +0200

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import Name from "./lib/Name.svelte";
+
+  let compName: Name;
+
+  onMount(() => {
+    // Type (mouse hover on `name`):   string
+    console.log(compName.name_);
+    // Type (mouse hover on `name`):   any
+    console.log(compName.name);
+  });
+</script>
+
+<Name bind:this={compName} />
---
+++ src/lib/Name.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  const name = "Vittorio Alfieri";
+
+  export const name_ = name;
+  export { name };
+</script>
+
+<p>
+  Hello {name}!
+</p>
